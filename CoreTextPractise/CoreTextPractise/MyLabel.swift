93fb63133f44699907a14441a87d6268ac3547e6
VassilyChi <cjfire@sina.cn>
2016-07-07 09:44:14 +0800

    项目初始化

---
+++ CoreTextPractise/CoreTextPractise/MyLabel.swift
@@ -0,0 +1,15 @@
+//
+//  MyLabel.swift
+//  CoreTextPractise
+//
+//  Created by Chi jie on 16/7/7.
+//  Copyright © 2016年 Vread. All rights reserved.
+//
+
+import UIKit
+
+class MyLabel: UILabel {
+
+    
+
+}
---
+++ CoreTextPractise/CoreTextPractise/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CoreTextPractise
+//
+//  Created by Chi jie on 16/7/7.
+//  Copyright © 2016年 Vread. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+
+}
+
