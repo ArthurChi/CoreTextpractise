97c607c2e9bf5dfae6ebb138f7ec00f6a474e034
VassilyChi <cjfire@sina.cn>
2016-07-08 14:43:36 +0800

    swift的调不好, 写个OC的

---
+++ CoreTextPractise/CoreTextPractise/ClickLabel.h
@@ -0,0 +1,13 @@
+//
+//  ClickLabel.h
+//  CoreTextPractise
+//
+//  Created by Chi jie on 16/7/8.
+//  Copyright © 2016年 Vread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClickLabel : UILabel
+
+@end
---
+++ CoreTextPractise/CoreTextPractise/ClickLabel.m
@@ -0,0 +1,21 @@
+//
+//  ClickLabel.m
+//  CoreTextPractise
+//
+//  Created by Chi jie on 16/7/8.
+//  Copyright © 2016年 Vread. All rights reserved.
+//
+
+#import "ClickLabel.h"
+
+@implementation ClickLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
