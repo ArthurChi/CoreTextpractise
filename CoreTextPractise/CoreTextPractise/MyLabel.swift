fc159129537ee04c22703c5b3412907be23a9cd9
cjfire <cjfire@sina.cn>
2016-12-31 11:29:40 +0800

    异步绘制

--- CoreTextPractise/CoreTextPractise/MyLabel.swift
+++ CoreTextPractise/CoreTextPractise/MyLabel.swift
@@ -14,116 +14,128 @@ class MyLabel: UILabel {
     var imgName = "badge_new"
     let kClickeableAttributeString = "ClickeableAttributeString"
     lazy var content = NSMutableAttributedString()
+    let queue = DispatchQueue(label: "drawQueue") // 这里需要是串行队列
     
     override func draw(_ rect: CGRect) {
         
-        if let contentAttr = attributedText {
-            content.append(contentAttr)
-        }
-        
-        build()
-        
-        let context = UIGraphicsGetCurrentContext()
-        
-        // 可以理解为 - 要旋转画布了
-        context?.saveGState()
-        
-        // 旋转画布
-        context!.textMatrix = CGAffineTransform.identity
-        context?.translateBy(x: 0, y: rect.size.height)
-        context?.scaleBy(x: 1.0, y: -1.0)
-        
-        /*
-         * 1. 通过AttributeString->CTFramesetter
-         * 2. 创建路径, 你可以做点形变, 可以修改渲染的路径
-         * 3. 通过以上两个对象, 创建CTFrame
-         * 4. 将CTFrame对象渲染上去
-         */
-        
-        let frameSetter = CTFramesetterCreateWithAttributedString(content as CFAttributedString)
-        
-        let path = CGMutablePath()
-        let bezierPath = UIBezierPath(ovalIn: CGRect(x: 50, y: UIScreen.main.bounds.size.height - 250, width: 100, height: 200))
-        path.addRect(bounds)
-        
-        // 比如, 要在这里加一个椭圆. 然后可以在这个区域画一个图片.
-        path.addPath(bezierPath.cgPath)
-        
-        frameAttr = CTFramesetterCreateFrame(frameSetter, CFRange(location: 0, length: content.length), path, nil)
-        
-        CTFrameDraw(frameAttr, context!)
-        
-        
-        
-        
-        // 属性文字渲染完毕
-        
-        // 这里要理解一个模型, CTFrame包含CTLine, CTLine包含CTRun
-        
-        /*
-         * 1. 从CTFrame中获取所有的CTLine
-         * 2. 建立一个CGPoint的数组, 用来存放没个CTLine的原点
-         * 3. 遍历所有的CTLine
-         * 4. 遍历当前CTLine中的CTRun
-         * 5. 从CTRun中获取属性字典中的值(这里需要知道CTRun是什么)
-         * 6. 找到后, 用Core Graphics画上去
-         */
-        
-        let lines = CTFrameGetLines(frameAttr) as Array
-        var lineOrigins = [CGPoint](repeating: CGPoint.zero, count: lines.count)
-        CTFrameGetLineOrigins(frameAttr, CFRange(location: 0, length: 0), &lineOrigins)
-        
-        for i in 0..<lines.count {
+        queue.async {
             
-            var
-            lineAscent = CGFloat(),
-            lineDescent = CGFloat(),
-            lineLeading = CGFloat()
+            if let contentAttr = self.attributedText {
+                self.content.append(contentAttr)
+            }
             
-            let line = lines[i] as! CTLine
-            CTLineGetTypographicBounds(line, &lineAscent, &lineDescent, &lineLeading)
+            self.build()
             
-            let runs = CTLineGetGlyphRuns(line) as Array
+            UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.layer.isOpaque, UIScreen.main.scale)
+            guard let context = UIGraphicsGetCurrentContext() else {
+                UIGraphicsEndImageContext()
+                return
+            }
             
-            for j in 0..<runs.count {
+            // 可以理解为 - 要旋转画布了
+            context.saveGState()
+            
+            // 旋转画布
+            context.textMatrix = CGAffineTransform.identity
+            context.translateBy(x: 0, y: rect.size.height)
+            context.scaleBy(x: 1.0, y: -1.0)
+            
+            /*
+             * 1. 通过AttributeString->CTFramesetter
+             * 2. 创建路径, 你可以做点形变, 可以修改渲染的路径
+             * 3. 通过以上两个对象, 创建CTFrame
+             * 4. 将CTFrame对象渲染上去
+             */
+            
+            let frameSetter = CTFramesetterCreateWithAttributedString(self.content as CFAttributedString)
+            
+            let path = CGMutablePath()
+            let bezierPath = UIBezierPath(ovalIn: CGRect(x: 50, y: UIScreen.main.bounds.size.height - 250, width: 100, height: 200))
+            path.addRect(self.bounds)
+            
+            // 比如, 要在这里加一个椭圆. 然后可以在这个区域画一个图片.
+            path.addPath(bezierPath.cgPath)
+            
+            self.frameAttr = CTFramesetterCreateFrame(frameSetter, CFRange(location: 0, length: self.content.length), path, nil)
+            CTFrameDraw(self.frameAttr, context)
+            
+            
+            // 属性文字渲染完毕
+            
+            // 这里要理解一个模型, CTFrame包含CTLine, CTLine包含CTRun
+            
+            /*
+             * 1. 从CTFrame中获取所有的CTLine
+             * 2. 建立一个CGPoint的数组, 用来存放没个CTLine的原点
+             * 3. 遍历所有的CTLine
+             * 4. 遍历当前CTLine中的CTRun
+             * 5. 从CTRun中获取属性字典中的值(这里需要知道CTRun是什么)
+             * 6. 找到后, 用Core Graphics画上去
+             */
+            
+            let lines = CTFrameGetLines(self.frameAttr) as Array
+            var lineOrigins = [CGPoint](repeating: CGPoint.zero, count: lines.count)
+            CTFrameGetLineOrigins(self.frameAttr, CFRange(location: 0, length: 0), &lineOrigins)
+            
+            for i in 0..<lines.count {
                 
                 var
-                runAscent = CGFloat(),
-                runDescent = CGFloat()
+                lineAscent = CGFloat(),
+                lineDescent = CGFloat(),
+                lineLeading = CGFloat()
                 
-                let run = runs[j] as! CTRun
+                let line = lines[i] as! CTLine
+                CTLineGetTypographicBounds(line, &lineAscent, &lineDescent, &lineLeading)
                 
-                var runRect = CGRect.zero
-                runRect.size.width = CGFloat(CTRunGetTypographicBounds(run, CFRangeMake(0, 0), &runAscent, &runDescent, nil))
+                let runs = CTLineGetGlyphRuns(line) as Array
                 
-                // 获取当前CTRun的Range, 用来控制画图所用的X
-                let strRangeLocation = CTRunGetStringRange(run).location
-                // 获取当前CTLine的原点, 用来控制画图所用的Y
-                let lineOrigin = lineOrigins[i]
-                // 算出绘图所需的Rect
-                runRect = CGRect(x: lineOrigin.x + CTLineGetOffsetForStringIndex(line, strRangeLocation, nil), y: lineOrigin.y - runDescent, width: runRect.size.width, height: runAscent - runDescent)
-                
-                // 这里是要取出刚才你起的attribute的名字
-                let attributes = CTRunGetAttributes(run)
-                let imageName = (attributes as NSDictionary)[imgName]
-                
-                // 生成Image并渲染
-                if let imageName = imageName as? String {
-                    if let image = UIImage(named: imageName) {
-                        
-                        var imageDrawRect = CGRect.zero
-                        imageDrawRect.size = CGSize(width: 30, height: 30)
-                        imageDrawRect.origin.x = runRect.origin.x + lineOrigin.x
-                        imageDrawRect.origin.y = lineOrigin.y
-                        context?.draw(image.cgImage!, in: imageDrawRect)
+                for j in 0..<runs.count {
+                    
+                    var
+                    runAscent = CGFloat(),
+                    runDescent = CGFloat()
+                    
+                    let run = runs[j] as! CTRun
+                    
+                    var runRect = CGRect.zero
+                    runRect.size.width = CGFloat(CTRunGetTypographicBounds(run, CFRangeMake(0, 0), &runAscent, &runDescent, nil))
+                    
+                    // 获取当前CTRun的Range, 用来控制画图所用的X
+                    let strRangeLocation = CTRunGetStringRange(run).location
+                    // 获取当前CTLine的原点, 用来控制画图所用的Y
+                    let lineOrigin = lineOrigins[i]
+                    // 算出绘图所需的Rect
+                    runRect = CGRect(x: lineOrigin.x + CTLineGetOffsetForStringIndex(line, strRangeLocation, nil), y: lineOrigin.y - runDescent, width: runRect.size.width, height: runAscent - runDescent)
+                    
+                    // 这里是要取出刚才你起的attribute的名字
+                    let attributes = CTRunGetAttributes(run)
+                    let imageName = (attributes as NSDictionary)[self.imgName]
+                    
+                    // 生成Image并渲染
+                    if let imageName = imageName as? String {
+                        if let image = UIImage(named: imageName) {
+                            
+                            var imageDrawRect = CGRect.zero
+                            imageDrawRect.size = CGSize(width: 30, height: 30)
+                            imageDrawRect.origin.x = runRect.origin.x + lineOrigin.x
+                            imageDrawRect.origin.y = lineOrigin.y
+                            context.draw(image.cgImage!, in: imageDrawRect)
+                        }
                     }
+                    
                 }
-                
+            }
+            
+            // 把刚旋转的画布转回来
+            context.restoreGState()
+            
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            DispatchQueue.main.async {
+                self.layer.contents = image?.cgImage
             }
         }
-        
-        // 把刚旋转的画布转回来
-        context?.restoreGState()
     }
     
     fileprivate func build() {
--- CoreTextPractise/CoreTextPractise/ViewController.swift
+++ CoreTextPractise/CoreTextPractise/ViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
-    @IBOutlet weak var showLabel: UILabel!
+    @IBOutlet weak var fpsLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var lastTime: CFTimeInterval = 0
+    var count: Double = 0
+    
+    lazy var link: CADisplayLink = {
+        
+        let link = CADisplayLink(target: self, selector: #selector(ViewController.fpsCalculater(link:)))
+        
+        return link
+    }()
+    
+    var attrbuteStr: NSAttributedString = {
+        
+        let attrbuteStr = NSMutableAttributedString(string: "这是一个测试文字, 来个图片, 还有点击事件, 多复制一些, 看看折行效果, cpoy1: 这是一个测试文字, 来个图片, 还有点击事件, cpoy2: 这是一个测试文字, 来个图片, 还有点击事件, cpoy3: 这是一个测试文字, 来个图片, 还有点击事件, 😀😖😐😣😡🚖🚌🚋🎊💖💗💛💙🏨🏦🏫")
+        
+        attrbuteStr.addAttributes([NSFontAttributeName: UIFont.systemFont(ofSize: 20)], range: NSRange(location: 0, length: attrbuteStr.length))
+        
+        return attrbuteStr
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let attrbuteStr = NSMutableAttributedString(string: "这是一个测试文字, 来个图片, 还有点击事件, 多复制一些, 看看折行效果, cpoy1: 这是一个测试文字, 来个图片, 还有点击事件, cpoy2: 这是一个测试文字, 来个图片, 还有点击事件, cpoy3: 这是一个测试文字, 来个图片, 还有点击事件, 这是一个测试文字, 来个图片, 还有点击事件, 多复制一些, 看看折行效果, cpoy1: 这是一个测试文字, 来个图片, 还有点击事件, cpoy2: 这是一个测试文字, 来个图片, 还有点击事件, cpoy3: 这是一个测试文字, 来个图片, 还有点击事件, 这是一个测试文字, 来个图片, 还有点击事件, 多复制一些, 看看折行效果, cpoy1: 这是一个测试文字, 来个图片, 还有点击事件, cpoy2: 这是一个测试文字, 来个图片, 还有点击事件, cpoy3: 这是一个测试文字, 来个图片, 还有点击事件, 这是一个测试文字, 来个图片, 还有点击事件, 多复制一些, 看看折行效果")
+        link.add(to: RunLoop.main, forMode: RunLoopMode.commonModes)
+        tableView.dataSource = self
+//        showLabel.attributedText = attrbuteStr
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 500
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "\(tableView.classForCoder)") {
+            return cell
+        } else {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "\(tableView.classForCoder)")
+
+            let showLabel = MyLabel()
+            showLabel.attributedText = attrbuteStr
+            cell.contentView.addSubview(showLabel)
+            showLabel.frame = cell.contentView.bounds
+            
+            return cell
+        }
+    }
+    
+    @objc private func fpsCalculater(link: CADisplayLink) {
+        let delta = link.timestamp - lastTime
         
-        attrbuteStr.addAttributes([NSFontAttributeName: UIFont.systemFont(ofSize: 20)], range: NSRange(location: 0, length: attrbuteStr.length))
+        count += 1
+        if delta < 1 {
+            return
+        }
         
-        showLabel.attributedText = attrbuteStr
+        lastTime = link.timestamp
+        fpsLabel.text = "\(count / delta)"
+        count = 0
     }
 }
 
